dcb6046726d5c5610d7fe29601513119b102d984
MirellePeralta <a.mirelleperalta@gmail.com>
2023-09-18 00:42:09 -0600

    Nueve

--- Proyecto_Inventarios/ProyectoInventarios_All/script.js
+++ Proyecto_Inventarios/ProyectoInventarios_All/script.js
@@ -1,22 +1,74 @@
-document.getElementById('productForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+function agregarProducto() {
+    const nombre = document.getElementById('nombre').value;
+    const descripcion = document.getElementById('descripcion').value;
+    const precio = document.getElementById('precio').value;
+    const stock = document.getElementById('stock').value;
+    const codigo = document.getElementById('codigo').value;
 
-    var nombre = document.getElementById('nombre').value;
-    var descripcion = document.getElementById('descripcion').value;
-    var precio = document.getElementById('precio').value;
-    var stock = document.getElementById('stock').value;
-    var code = document.getElementById('code').value;
+    const inventario = document.getElementById('inventario');
 
-    var newRow = document.createElement('tr');
+    const newRow = document.createElement('tr');
     newRow.innerHTML = `
         <td>${nombre}</td>
         <td>${descripcion}</td>
         <td>${precio}</td>
         <td>${stock}</td>
-        <td>${code}</td>
+        <td>${codigo}</td>
+        <td>
+            <button type="button" onclick="sumarStockProducto(this)">+ Stock</button>
+            <button type="button" onclick="restarStockProducto(this)">- Stock</button>
+        </td>
     `;
 
-    document.getElementById('productTable').getElementsByTagName('tbody')[0].appendChild(newRow);
+    inventario.appendChild(newRow);
+}
 
-    document.getElementById('productForm').reset();
-});
+function sumarStockProducto(button) {
+    const row = button.parentElement.parentElement;
+    const stockCell = row.querySelector('td:nth-child(4)');
+    stockCell.textContent = Number(stockCell.textContent) + 1;
+}
+
+function restarStockProducto(button) {
+    const row = button.parentElement.parentElement;
+    const stockCell = row.querySelector('td:nth-child(4)');
+    if (Number(stockCell.textContent) > 0) {
+        stockCell.textContent = Number(stockCell.textContent) - 1;
+    }
+}
+
+function sumarStock() {
+    const codigo = document.getElementById('codigoStock').value;
+    const cantidad = Number(document.getElementById('cantidad').value);
+
+    const inventario = document.getElementById('inventario').getElementsByTagName('tr');
+    for (let i = 1; i < inventario.length; i++) {
+        const codigoProducto = inventario[i].getElementsByTagName('td')[4].textContent;
+        if (codigo === codigoProducto) {
+            const stockCell = inventario[i].getElementsByTagName('td')[3];
+            stockCell.textContent = Number(stockCell.textContent) + cantidad;
+            break;
+        }
+    }
+}
+
+function restarStock() {
+    const codigo = document.getElementById('codigoStock').value;
+    const cantidad = Number(document.getElementById('cantidad').value);
+
+    const inventario = document.getElementById('inventario').getElementsByTagName('tr');
+    for (let i = 1; i < inventario.length; i++) {
+        const codigoProducto = inventario[i].getElementsByTagName('td')[4].textContent;
+        if (codigo === codigoProducto) {
+            const stockCell = inventario[i].getElementsByTagName('td')[3];
+            if (Number(stockCell.textContent) >= cantidad) {
+                stockCell.textContent = Number(stockCell.textContent) - cantidad;
+            }
+            break;
+        }
+    }
+}
+
+
+
+
