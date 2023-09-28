a9252b97dcdda9288a37024202f0a8fc5fe18b4a
MirellePeralta <a.mirelleperalta@gmail.com>
2023-09-27 22:58:42 -0600

    Veintidos

--- Proyecto_Inventarios/ProyectoInventarios_All/script.js
+++ Proyecto_Inventarios/ProyectoInventarios_All/script.js
@@ -53,6 +53,7 @@ function generarCodigo() {
 function agregarProducto() {
     const nombre = document.getElementById('nombre').value;
     const descripcion = document.getElementById('descripcion').value;
+    // Acá Inserte algo nuevo
     const seccion = document.getElementById('seccion').value;
     const precio = document.getElementById('precio').value;
     const stock = document.getElementById('stock').value;
@@ -75,25 +76,10 @@ function agregarProducto() {
     `;
 
     inventario.appendChild(newRow);
+    // Mostrar alerta
+    alert("Producto añadido con éxito.");
 
-    // Guardar en localStorage
-    const producto = {
-        nombre: nombre,
-        descripcion: descripcion,
-        seccion: seccion,
-        precio: precio,
-        stock: stock,
-        codigo: codigo
-    };
-
-    // Verificar si ya hay productos guardados
-    let productosGuardados = JSON.parse(localStorage.getItem('productos')) || [];
-    productosGuardados.push(producto);
-
-    // Guardar la lista actualizada en localStorage
-    localStorage.setItem('productos', JSON.stringify(productosGuardados));
 }
-
 
 function sumarStockProducto(button) {
     const row = button.parentElement.parentElement;
@@ -122,6 +108,9 @@ function sumarStock() {
             break;
         }
     }
+
+    // Mostrar alerta
+    alert("El producto ha sido correctamente modificado.");
 }
 
 function restarStock() {
@@ -139,6 +128,8 @@ function restarStock() {
             break;
         }
     }
+        // Mostrar alerta
+        alert("El producto ha sido correctamente modificado.");
 }
 function mostrarContenido(tab) {
     const tabs = document.querySelectorAll('.contenido-tab');
@@ -171,6 +162,7 @@ function crearTablaSucursal() {
     `;
     sucursal.tabla = nuevaTabla.querySelector('table');
     sucursalesContenido.appendChild(nuevaTabla);
+    alert("Sucursal añadida con éxito.");
 }
 
 function distribuirStock(sucursalNombre) {
@@ -205,8 +197,146 @@ function distribuirStock(sucursalNombre) {
                 stock: cantidadDistribuida,
                 codigo: codigo
             });
-
+            
             break;
         }
     }
 }
+
+let productoSeleccionado = null;
+
+function buscarProducto() {
+    const codigo = document.getElementById('codigoProducto').value;
+    const inventario = document.getElementById('inventario').getElementsByTagName('tr');
+
+    for (let i = 1; i < inventario.length; i++) {
+        const codigoProducto = inventario[i].getElementsByTagName('td')[5].textContent;
+
+        if (codigo === codigoProducto) {
+            const nombre = inventario[i].getElementsByTagName('td')[0].textContent;
+            const descripcion = inventario[i].getElementsByTagName('td')[1].textContent;
+            const seccion = inventario[i].getElementsByTagName('td')[2].textContent;
+            const precio = inventario[i].getElementsByTagName('td')[3].textContent;
+            const stock = inventario[i].getElementsByTagName('td')[4].textContent;
+
+            productoSeleccionado = {
+                nombre: nombre,
+                descripcion: descripcion,
+                seccion: seccion,
+                precio: precio,
+                stock: stock,
+                codigo: codigo
+            };
+
+            // Mostrar los detalles del producto en la interfaz de modificación
+            document.getElementById('nombreMod').value = nombre;
+            document.getElementById('descripcionMod').value = descripcion;
+            document.getElementById('seccionMod').value = seccion;
+            document.getElementById('precioMod').value = precio;
+            document.getElementById('stockMod').value = stock;
+
+            // Mostrar la interfaz de modificación
+            document.getElementById('modificarProducto').style.display = 'block';
+            return; // Salir de la función después de encontrar el producto
+        }
+    }
+
+    // Si llegamos aquí, significa que no se encontró el producto con el código ingresado
+    alert("No se encontró ningún producto con el código proporcionado.");
+}
+
+function confirmarModificacion() {
+    // Mostrar cuadro de diálogo de confirmación
+    const confirmacion = confirm("¿Está seguro de que quiere modificar este producto?");
+
+    if (confirmacion) {
+        // Modificar el producto en la tabla de inventario
+        const inventario = document.getElementById('inventario').getElementsByTagName('tr');
+
+        for (let i = 1; i < inventario.length; i++) {
+            const codigoProducto = inventario[i].getElementsByTagName('td')[5].textContent;
+
+            if (productoSeleccionado.codigo === codigoProducto) {
+                inventario[i].getElementsByTagName('td')[0].textContent = document.getElementById('nombreMod').value;
+                inventario[i].getElementsByTagName('td')[1].textContent = document.getElementById('descripcionMod').value;
+                inventario[i].getElementsByTagName('td')[2].textContent = document.getElementById('seccionMod').value;
+                inventario[i].getElementsByTagName('td')[3].textContent = document.getElementById('precioMod').value;
+                inventario[i].getElementsByTagName('td')[4].textContent = document.getElementById('stockMod').value;
+
+                // Ocultar la interfaz de modificación después de modificar el producto
+                document.getElementById('modificarProducto').style.display = 'none';
+
+                // Mostrar alerta de modificación exitosa
+                alert("Producto modificado con éxito.");
+                return; // Salir de la función después de modificar el producto
+            }
+        }
+    }
+}
+
+function cancelarModificacion() {
+    // Ocultar la interfaz de modificación si se cancela
+    document.getElementById('modificarProducto').style.display = 'none';
+}
+
+// HTML necesario:
+// - Un cuadro de texto con el ID 'codigoProducto'
+// - Un botón "Siguiente" que llama a la función buscarProducto
+// - Una interfaz de modificación con campos para nombre, descripción, sección, precio y stock, y un botón "Aceptar" que llama a la función confirmarModificacion, y un botón "Cancelar" que llama a la función cancelarModificacion
+// ...
+
+function buscarProductos() {
+    const criterio = document.getElementById('criterio').value;
+    const busqueda = document.getElementById('busqueda').value.toLowerCase();
+    const inventario = document.getElementById('inventario').getElementsByTagName('tr');
+    const resultadosDiv = document.getElementById('resultados');
+    resultadosDiv.innerHTML = '';
+
+    for (let i = 1; i < inventario.length; i++) {
+        const valorCelda = inventario[i].getElementsByTagName('td')[getIndiceCriterio(criterio)].textContent.toLowerCase();
+
+        if (valorCelda.includes(busqueda)) {
+            const nombre = inventario[i].getElementsByTagName('td')[0].textContent;
+            const descripcion = inventario[i].getElementsByTagName('td')[1].textContent;
+            const seccion = inventario[i].getElementsByTagName('td')[2].textContent;
+            const precio = inventario[i].getElementsByTagName('td')[3].textContent;
+            const stock = inventario[i].getElementsByTagName('td')[4].textContent;
+            const codigo = inventario[i].getElementsByTagName('td')[5].textContent;
+
+            resultadosDiv.innerHTML += `
+                <div>
+                    <h3>${nombre}</h3>
+                    <p>Descripción: ${descripcion}</p>
+                    <p>Sección: ${seccion}</p>
+                    <p>Precio: ${precio}</p>
+                    <p>Stock: ${stock}</p>
+                    <p>Código: ${codigo}</p>
+                </div>
+            `;
+        }
+    }
+}
+
+function getIndiceCriterio(criterio) {
+    switch (criterio) {
+        case 'nombre':
+            return 0;
+        case 'descripcion':
+            return 1;
+        case 'seccion':
+            return 2;
+        case 'precio':
+            return 3;
+        case 'stock':
+            return 4;
+        case 'codigo':
+            return 5;
+        default:
+            return 0;
+    }
+}
+
+// ...
+
+
+
